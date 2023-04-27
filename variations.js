34a1f8538d7bf26b41ee6d101b8029ae28878271
Balu <101409985+balu1006@users.noreply.github.com>
2023-04-27 23:45:51 +0530

    Add files via upload

---
+++ variations.js
@@ -0,0 +1,25 @@
+function getNumberOfChars(name) {
+  return name.length;
+}
+
+function getFirstChar(name) {
+  return name[0];
+}
+
+function getLastChar(name) {
+  return name[name.length - 1];
+}
+
+function getLower(name) {
+  return name.toLowerCase();
+}
+
+function getUpper(name) {
+  return name.toUpperCase();
+}
+
+function getCapitalized(name) {
+  return name[0].toUpperCase() + name.substring(1).toLowerCase();
+}
+
+export { getNumberOfChars, getFirstChar, getLastChar, getLower, getUpper, getCapitalized };
